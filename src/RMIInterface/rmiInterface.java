dcc6f4c1632ce8edc1ecb243c7981a35c09d6507
DaniBorisov <36665902+DaniBorisov@users.noreply.github.com>
2021-10-24 18:48:11 +0200

    Fixed basic connection, added SSL Sockets- still problem with handshaking :(

--- src/RMIInterface/rmiInterface.java
+++ src/RMIInterface/rmiInterface.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 
 public interface rmiInterface extends Remote {
 
-    public String test1() throws RemoteException;
+    public String test1(String name) throws RemoteException;
+
 
 }
